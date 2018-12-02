db02ea4d2917f4062c4c4052126b5e9bdde15cec
Vitor Lima <vitor@ferreiradelima.com>
2018-12-01 22:44:25 -0200

    node

--- node_service/pol_start.js
+++ node_service/pol_start.js
@@ -1,11 +1,7 @@
 const { spawn } = require('child_process');
 
-const subprocess = spawn('./pol', [], {
-   detached: true,
-   stdio: [ 'ignore', out, err ]
- });
-
- subprocess.on('close', (code, signal) => {
+const subprocess = spawn('./pol', []);
+subprocess.on('close', (code, signal) => {
    console.log(`child process terminated due to receipt of signal ${signal}`);
 
    process.exit();
