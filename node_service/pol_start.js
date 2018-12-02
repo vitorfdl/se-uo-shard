c03897459004440f1dd8fb1b0239d7ad505e74d0
Vitor Lima <vitor@ferreiradelima.com>
2018-12-01 23:01:17 -0200

    more fixes

--- node_service/pol_start.js
+++ node_service/pol_start.js
@@ -1,6 +1,9 @@
 const { spawn } = require('child_process');
 
-const subprocess = spawn('./pol', []);
+const subprocess = spawn('./pol', [], {
+   stdio: 'inherit',
+   shell: true
+ });
 
 function ServiceExited(code, signal) {
    console.log(`child process terminated due to receipt of signal ${signal}`);
@@ -10,10 +13,10 @@ function ServiceExited(code, signal) {
 subprocess.on('close', ServiceExited);
 subprocess.on('exit', ServiceExited);
 
-subprocess.stdout.on('data', (data) => {
-   console.log(`${data}`);
- });
+// subprocess.stdout.on('data', (data) => {
+//    console.log(`${data}`);
+//  });
  
- subprocess.stderr.on('data', (data) => {
-   console.error(`${data}`);
- });
+//  subprocess.stderr.on('data', (data) => {
+//    console.error(`${data}`);
+//  });
